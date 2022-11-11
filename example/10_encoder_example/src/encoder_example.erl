048a670a5069e79cc06659d41ee722bb24378e36
Bien Nguyen <nguyennhubientdh94@gmail.com>
2022-11-11 15:12:59 +0000

    change owner of encoder_example.erl

--- example/10_encoder_example/src/encoder_example.erl
+++ example/10_encoder_example/src/encoder_example.erl
@@ -1,7 +1,7 @@
 %
 % This file is part of AtomVM.
 %
-% Copyright 2022 Fred Dushin <fred@dushin.net>
+% Copyright 2022 Bien Nguyen <nguyennhubientdh94@gmail.com>
 %
 % Licensed under the Apache License, Version 2.0 (the "License");
 % you may not use this file except in compliance with the License.
