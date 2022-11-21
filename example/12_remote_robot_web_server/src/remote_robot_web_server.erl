70f002a0ba463f42726c670493531942d344075d
Bien Nguyen <nguyennhubientdh94@gmail.com>
2022-11-21 08:22:13 +0000

    support pwm

--- example/12_remote_robot_web_server/src/remote_robot_web_server.erl
+++ example/12_remote_robot_web_server/src/remote_robot_web_server.erl
@@ -22,12 +22,25 @@
 
 -export([start/0, handle_req/3]).
 
+-include("ledc.hrl").
+
 -define(MOTOR_1_PIN_1, 4).
 -define(MOTOR_1_PIN_2, 16).
+-define(MOTOR_1_EN, 17).
+
 -define(MOTOR_2_PIN_1, 5).
 -define(MOTOR_2_PIN_2, 18).
+-define(MOTOR_2_EN, 19).
+
+-define(LEDC_HS_MODE, ?LEDC_HIGH_SPEED_MODE).
+-define(LEDC_HS_TIMER, ?LEDC_TIMER_0).
+-define(LEDC_HS_CH0_CHANNEL, ?LEDC_CHANNEL_0).
+-define(LEDC_HS_CH1_CHANNEL, ?LEDC_CHANNEL_1).
+
+-define(LEDC_PWM, 6000). %half of 13bit
 
 start() ->
+    setup_pwm(),
     setup_motor(),
     Self = self(),
     Config = [
@@ -45,6 +58,38 @@ start() ->
         Error ->
             erlang:display(Error)
     end.
+
+setup_pwm() ->
+    LEDCHSTimer = [
+        {duty_resolution, ?LEDC_TIMER_13_BIT},
+        {freq_hz, 5000},
+        {speed_mode, ?LEDC_HS_MODE},
+        {timer_num, ?LEDC_HS_TIMER}
+    ],
+    ok = ledc:timer_config(LEDCHSTimer),
+    LEDCChannel = [
+        [
+            {channel, ?LEDC_HS_CH0_CHANNEL},
+            {duty, 0},
+            {gpio_num, ?MOTOR_1_EN},
+            {speed_mode, ?LEDC_HS_MODE},
+            {hpoint, 0},
+            {timer_sel, ?LEDC_HS_TIMER}
+        ],[
+            {channel, ?LEDC_HS_CH1_CHANNEL},
+            {duty, 0},
+            {gpio_num, ?MOTOR_2_EN},
+            {speed_mode, ?LEDC_HS_MODE},
+            {hpoint, 0},
+            {timer_sel, ?LEDC_HS_TIMER}
+        ]
+    ],
+    lists:foreach(
+        fun(ChannelConfig) ->
+            ok = ledc:channel_config(ChannelConfig)
+        end,
+        LEDCChannel
+    ).
 
 setup_motor() ->
     MotorPinList = [?MOTOR_1_PIN_1, ?MOTOR_1_PIN_2,
@@ -94,35 +139,35 @@ handle_req("GET", [], Conn) ->
 
 handle_req("POST", ["forward"], Conn) ->
     erlang:display(forward),
-    move(1, forward),
-    move(2, forward),
+    move(1, forward, ?LEDC_PWM),
+    move(2, forward, ?LEDC_PWM),
     Body = <<"<html><body><h1>anything</h1></body></html>">>,
     http_server:reply(200, Body, Conn);
 
 handle_req("POST", ["backward"], Conn) ->
     erlang:display(backward),
-    move(1, backward),
-    move(2, backward),
+    move(1, backward, ?LEDC_PWM),
+    move(2, backward, ?LEDC_PWM),
     Body = <<"<html><body><h1>anything</h1></body></html>">>,
     http_server:reply(200, Body, Conn);
 
 handle_req("POST", ["right"], Conn) ->
     erlang:display(right),
-    move(1, forward),
-    move(2, backward),
+    move(1, forward, ?LEDC_PWM),
+    move(2, backward, ?LEDC_PWM),
     Body = <<"<html><body><h1>anything</h1></body></html>">>,
     http_server:reply(200, Body, Conn);
 
 handle_req("POST", ["left"], Conn) ->
     erlang:display(left),
-    move(1, backward),
-    move(2, forward),
+    move(1, backward, ?LEDC_PWM),
+    move(2, forward, ?LEDC_PWM),
     Body = <<"<html><body><h1>anything</h1></body></html>">>,
     http_server:reply(200, Body, Conn);
 
 handle_req("POST", ["stop"], Conn) ->
     erlang:display(stop),
-    move(anything, stop),
+    move(anything, stop, 0),
     Body = <<"<html><body><h1>anything</h1></body></html>">>,
     http_server:reply(200, Body, Conn);
 
@@ -138,23 +183,35 @@ handle_req(Method, Path, Conn) ->
     Body = <<"<html><body><h1>Not Found</h1></body></html>">>,
     http_server:reply(200, Body, Conn).
 
-move(1, forward) ->
+move(1, forward, PWM) ->
     gpio:digital_write(?MOTOR_1_PIN_1, high),
-    gpio:digital_write(?MOTOR_1_PIN_2, low);
-move(1, backward) ->
+    gpio:digital_write(?MOTOR_1_PIN_2, low),
+    ok = ledc:set_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL, PWM),
+    ok = ledc:update_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL);   
+move(1, backward, PWM) ->
     gpio:digital_write(?MOTOR_1_PIN_1, low),
-    gpio:digital_write(?MOTOR_1_PIN_2, high);
-move(2, forward) ->
+    gpio:digital_write(?MOTOR_1_PIN_2, high),
+    ok = ledc:set_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL, PWM),
+    ok = ledc:update_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL);     
+move(2, forward, PWM) ->
     gpio:digital_write(?MOTOR_2_PIN_1, high),
-    gpio:digital_write(?MOTOR_2_PIN_2, low);
-move(2, backward) ->
+    gpio:digital_write(?MOTOR_2_PIN_2, low),
+    ok = ledc:set_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL, PWM),
+    ok = ledc:update_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL);     
+move(2, backward, PWM) ->
     gpio:digital_write(?MOTOR_2_PIN_1, low),
-    gpio:digital_write(?MOTOR_2_PIN_2, high);
-move(_, stop) ->
+    gpio:digital_write(?MOTOR_2_PIN_2, high),
+    ok = ledc:set_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL, PWM),
+    ok = ledc:update_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL);     
+move(_, stop, _PWM) ->
     gpio:digital_write(?MOTOR_1_PIN_1, low),
     gpio:digital_write(?MOTOR_1_PIN_2, low),
     gpio:digital_write(?MOTOR_2_PIN_1, low),
-    gpio:digital_write(?MOTOR_2_PIN_2, low).
+    gpio:digital_write(?MOTOR_2_PIN_2, low),
+    ok = ledc:set_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL, 0),
+    ok = ledc:update_duty(?LEDC_HS_MODE, ?LEDC_HS_CH0_CHANNEL),
+    ok = ledc:set_duty(?LEDC_HS_MODE, ?LEDC_HS_CH1_CHANNEL, 0),
+    ok = ledc:update_duty(?LEDC_HS_MODE, ?LEDC_HS_CH1_CHANNEL).    
 
 safe_list_to_integer(L) ->
     try erlang:list_to_integer(L) of
