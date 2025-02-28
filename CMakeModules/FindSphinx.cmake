0b471388f61edb6027e934e4a372127ebc312ca0
biennguyen94 <nguyennhubientdh94@gmail.com>
2025-02-28 20:06:51 +0700

    remove related sphinx files

--- CMakeModules/FindSphinx.cmake
+++
@@ -1,8 +0,0 @@
-find_program(SPHINX_PATH sphinx-build)
-
-if (SPHINX_PATH)
-    set(SPHINX_FOUND TRUE)
-    set(SPHINX_BUILD_EXECUTABLE "${SPHINX_PATH}")
-elseif(SPHINX_FIND_REQUIRED)
-    message(FATAL_ERROR "Sphinx command (spinx-build) not found")
-endif()
